b2fa425c9e45b7edceee1c72c83338fc7d35b39d
Jan Malakhovski <oxij@oxij.org>
2024-01-23 12:00:00 +0000

    bump version

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ with pkgs.python3Packages;
 
 buildPythonApplication (rec {
   pname = "imaparms";
-  version = "2.4.0";
+  version = "2.5.0";
   format = "pyproject";
 
   src = lib.cleanSourceWith {
