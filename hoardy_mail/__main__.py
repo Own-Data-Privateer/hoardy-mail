d555eeeb8c9151b4298fe9c6b90d5d0acfe01d81
Jan Malakhovski <oxij@oxij.org>
2025-01-02 16:30:00 +0000

    *: make `pylint` happy by adding module docstrings

--- hoardy_mail/__main__.py
+++ hoardy_mail/__main__.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+"""`main()`."""
+
 import dataclasses as _dc
 import decimal
 import fcntl as _fcntl
