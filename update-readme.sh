228a928463ab4780bf747f607d2b28abf68531bc
Jan Malakhovski <oxij@oxij.org>
2025-01-22 11:35:00 +0000

    update-readme.sh: simplify

--- update-readme.sh
+++ update-readme.sh
@@ -1,10 +1,13 @@
 #!/bin/sh -e
 
-sed -n "0,/# Usage/ p" README.md > README.new
-echo >> README.new
-python3 -m hoardy_mail.__main__ --help --markdown | sed '
+{
+    sed -n "/# What is/,/# Usage/ p" README.md
+    echo
+
+    python3 -m hoardy_mail.__main__ --help --markdown | sed '
 s/^\(#\+\) /#\1 /
 s/^\(#\+\) \(hoardy-mail[^[({]*\) [[({].*/\1 \2/
-' >> README.new
+'
+} > README.new
 mv README.new README.md
 pandoc -s -V pagetitle=README -f markdown -t html README.md > README.html
